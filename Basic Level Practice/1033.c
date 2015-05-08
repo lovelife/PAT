235401c77263eccdf6e24e2c456fae0abc58b7ec
youwei <passion.py@gmail.com>
2015-05-08 14:57:49 +0800

    1033 AC

--- Basic Level Practice/1033.c
+++ Basic Level Practice/1033.c
@@ -1,62 +1,36 @@
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
+#include<stdio.h>
+#include<ctype.h>
 
-int main(int argc, char *argv[]) 
+int main()
 {
-    char lo[26] = {0}, di[10] = {0}, ch[4] = {0}, space = 0;
-    char badkey[100001] = {0}, instr[100001] = {0};
+    int badkey[256] = {0}, flag = 1, i;
+    char ch, str[100001] = {0};
     
-    //scanf("%s %s",badkey,instr);
-    gets(badkey);
-    gets(instr);
-    
-    int i , isnull = 1;
-    for(i = 0; i < strlen(badkey); i++)
+    gets(str);
+    for(i = 0; (ch = str[i]) != 0; i++)
     {
-        if(isdigit(badkey[i]))
-            di[badkey[i] - '0'] = 1;
-        else if(isupper(badkey[i]))
-        {
-            lo[badkey[i] - 'A'] = 1;
-        }
-        else if(badkey[i] == '_')
-        {
-            space = 1;
-        }
-        else {// + , - .
-            ch[ badkey[i] - '+'] = 1;
-        }
-                
+        badkey[ch] = 1;
+        if(isupper(ch)) badkey[ch - 'A' + 'a'] = 1;
     }
-    for (i = 0; i < strlen(instr); i++) 
+    
+    if(badkey['+'])
     {
-        if(isdigit(instr[i]) && !di[instr[i]-'0'])
+        for(ch = 'A'; ch <= 'Z'; ch++)
         {
-            printf("%c",instr[i]);
-            isnull = 0;
-        }    
-        else if (islower(instr[i]) && !lo[instr[i]-'a']) {
-            printf("%c",instr[i]);
-            isnull = 0;
-        }  
-        else if (isupper(instr[i]) && !ch[0] && !lo[instr[i]-'A']) {
-            printf("%c",instr[i]);
-            isnull = 0;
-        }
-        else if ( (instr[i] == '_') && !space) {
-            printf("_");
-            isnull = 0;
-        }  
-        else if( instr[i] >= '+' && instr[i] <= '.' && !ch[instr[i] - '+'])
-        {
-            printf("%c",instr[i]);
-            isnull = 0;
+            badkey[ch] = 1;
         }
     }
-    if(isnull) printf("\n");
-    return 0;    
+    
+    gets(str);
+    for(i = 0; (ch = str[i]) != 0; i++)
+    {
+        if(!badkey[ch]) {
+            printf("%c",ch);
+            flag = 0;
+        }
+    }  
+    
+    if(flag) printf("\n");
+    
+    return 0;
 }
-
-
-
